e7474091e94e8bc7690476db83c44c5de0578589
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2020-09-27 17:23:08 -0700

    se conecto spotify api con el state para ver al usuario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import SpotifyWebApi from "spotify-web-api-js";
+
 import './App.css';
 
 import Login from "./Login";
+import Player from './Player';
+import { getTokenFromUrl } from "./spotify";
 
+const  spotify = new SpotifyWebApi();
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() =>{
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+  
+    if( _token ){
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user =>{
+        console.log(user);
+      })
+    }
+
+  }, []);
+
   return (
     <div className="app">
-      <Login />
+      {token ? (<Player />):(
+        <Login />) }
     </div>
   );
 }
